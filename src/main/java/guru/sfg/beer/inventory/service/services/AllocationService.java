aa990dbf44bf3bcb7926b44baba9d6f0dca9d95a
marcinpeck <marcinpeck@gmail.com>
2021-04-22 20:53:25 +0200

    added DeallocateOrderRequest

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
@@ -9,5 +9,5 @@ public interface AllocationService {
 
     Boolean allocateOrder(BeerOrderDto beerOrderDto);
 
-//    void deallocateOrder(BeerOrderDto beerOrderDto);
+    void deallocateOrder(BeerOrderDto beerOrderDto);
 }
--- src/main/java/guru/sfg/brewery/model/events/DeallocateOrderRequest.java
+++ src/main/java/guru/sfg/brewery/model/events/DeallocateOrderRequest.java
@@ -1,18 +1,18 @@
 package guru.sfg.brewery.model.events;
 
+import guru.sfg.brewery.model.BeerOrderDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * Created by marcin.peck on 14.04.2021
+ * Created by marcin.peck on 22.04.2021
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class NewInventoryEvent{
-    static final long serialVersionUID = -5781515597148163111L;
-    private BeerDto beerDto;
+public class DeallocateOrderRequest {
+    private BeerOrderDto beerOrderDto;
 }
